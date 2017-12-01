7bb977d82354303422258cac3e8571c773c50d1c
Hugo Fuentes <alverik@Macaria.local>
2017-12-01 17:24:27 -0600

    Se cambia la versión de pi4j

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
-
-BIN_PATH="/home/pi/raspberryJ-bin"
-CODE_PATH="/home/pi/raspberryJ"
+BASE_PATH="/home/pi/raspberryJ"
+BIN_PATH="$BASE_PATH/raspberryJ-bin"
+CODE_PATH="$BASE_PATH/raspberryJ-code"
 
 createDirectories() {
     echo "Creating Directories"
+
+    if [ ! -d "$BASE_PATH" ]; then
+        echo "Creating base path"
+        mkdir "$BASE_PATH"
+    fi
+
+    cd "$BASE_PATH"
 
     if [ ! -d "$BIN_PATH" ]; then
         echo "Creating binary path"
@@ -15,18 +22,15 @@ createDirectories() {
         echo "Creating log path"
         mkdir "$BIN_PATH/log"
     fi
-
-    if [ "$1" == "code" ]; then
-        echo "Creating source path"
-        if [ ! -d "$CODE_PATH" ]; then
-            mkdir "$CODE_PATH"
-            cd "$CODE_PATH"
-            echo "Cloning Source Code"
-            git clone https://github.com/Cuixe/raspberryJ.git
-        fi
-    fi
 }
 
+createSourceCode() {
+    if [ ! -d "$CODE_PATH" ]; then
+        echo "Cloning Source Code"
+        git clone https://github.com/Cuixe/raspberryJ.git
+        mv raspberryJ raspberryJ-code
+    fi
+}
 binaryDeploy() {
     createDirectories
     echo "Deploying Binary"
@@ -39,6 +43,7 @@ binaryDeploy() {
 
 codeDeploy() {
     createDirectories
+    createSourceCode
     cd "$CODE_PATH"
     echo "Getting last version"
     git pull
@@ -50,8 +55,10 @@ codeDeploy() {
 }
 
 if [ "$1" == "code" ]; then
+    echo "DEPLOYING SOURCE CODE"
     codeDeploy
 elif [ "$1" == "binary" ]; then
+    echo "DEPLOYING BINARY"
     binaryDeploy
 else
     echo "INVALID OPTION"
--- publish.sh
+++ publish.sh
@@ -3,7 +3,7 @@
 sendScript() {
     echo "Sending deploy script"
     scp deploy.sh raspberry1:./
-    ssh raspberry1 'chmod +x /home/pi/deploy.sh'
+    ssh raspberry1 'chmod +x deploy.sh'
     echo "DONE Sending deploy script"
 }
 
@@ -16,14 +16,14 @@ publishBinary() {
     scp build/raspberry-1.0-SNAPSHOT.tgz raspberry1:./raspberryJ-bin
     echo "DONE Sending binary"
     echo "Running Deploy"
-    ssh raspberry1 './home/pi/deploy.sh binary'
+    ssh raspberry1 './deploy.sh binary'
     echo "DONE Running Deploy"
 }
 
 publishCode () {
     sendScript
     echo "Running Deploy"
-    ssh raspberry1 './home/pi/deploy.sh code'
+    ssh raspberry1 './deploy.sh code'
     echo "DONE Running Deploy"
 }
 
