b9cd6e230f861571125e628008190d086230279c
Hugo Fuentes <alverik@Macaria.local>
2017-12-02 10:06:04 -0600

    Se asigna pin a led para prevenir fallas

--- src/main/java/org/cuixe/raspberry/Main.java
+++ src/main/java/org/cuixe/raspberry/Main.java
@@ -13,11 +13,27 @@ public class Main {
     private static GpioController gpio = null;
     private static List<Integer> forbiddenPins = new ArrayList<>();
 
+    private static Map<Integer, Pin> ledPin = new HashMap<>();
+    static {
+        ledPin.put(1, RaspiPin.GPIO_15);
+        ledPin.put(2, RaspiPin.GPIO_16);
+        ledPin.put(3, RaspiPin.GPIO_01);
+        ledPin.put(4, RaspiPin.GPIO_04);
+        ledPin.put(5, RaspiPin.GPIO_05);
+        ledPin.put(6, RaspiPin.GPIO_06);
+        ledPin.put(7, RaspiPin.GPIO_10);
+        ledPin.put(8, RaspiPin.GPIO_11);
+        ledPin.put(9, RaspiPin.GPIO_26);
+        ledPin.put(10, RaspiPin.GPIO_27);
+        ledPin.put(11, RaspiPin.GPIO_28);
+        ledPin.put(12, RaspiPin.GPIO_29);
+    }
+
     public static void main(String[] args) {
 
         int option;
         int mode;
-        int pin = 0;
+        int led = 0;
         Scanner lec = new Scanner(System.in);
         initialize();
         while(true) {
@@ -26,34 +42,35 @@ public class Main {
             if (option == 0) {
                 break;
             } else if(option == 2) {
-                System.out.println("Enter pin: 0=exit");
-                pin = lec.nextInt();
-                if (pin == 0) {
+                System.out.println("Enter LED: 0=exit");
+                led = lec.nextInt();
+                if (led == 0) {
                     break;
                 }
             } else if (option == 1) {
-                pin = -1;
+                led = -1;
             }
             System.out.println("Enter mode: 1=turnOn, 2=turnOff, 3 turnOn-turnOff, 0=exit");
             mode = lec.nextInt();
             if (mode == 0) {
                 break;
             }
-            work(pin, mode);
+            work(led, mode);
         }
         gpio.shutdown();
     }
 
-    private static void work(int pin, int mode) {
-        if(pin != -1) {
-            executeMode(pin, mode);
+    private static void work(int led, int mode) {
+        if(led != -1) {
+            executeMode(led, mode);
         } else {
-            pins.keySet().forEach((tmp) -> executeMode(tmp, mode));
+            ledPin.keySet().forEach((tmp) -> executeMode(tmp, mode));
         }
     }
 
-    private static void executeMode(int pin, int mode) {
-        System.out.println("Operating pin: " + pin + " Mode " + mode);
+    private static void executeMode(int led, int mode) {
+        Pin pin = ledPin.get(led);
+        System.out.println("Operating led: " + led + " Mode " + mode);
         try {
             if (mode == 1)
                 turnOn(pin);
@@ -72,35 +89,30 @@ public class Main {
         gpio = GpioFactory.getInstance();
         forbiddenPins.add(20);
         System.out.println("Inicializando PINS");
-        Pin[] allPins = RaspiPin.allPins();
-        for (int i = 0; i< allPins.length; i++) {
-            Pin pin = allPins[i];
-            GpioPinDigitalOutput gpioPin = gpio.provisionDigitalOutputPin(pin, pin.getName(), PinState.HIGH);
+        ledPin.entrySet().forEach((entry) -> {
+            entry.getKey();
+            Pin pin = entry.getValue();
+            GpioPinDigitalOutput gpioPin = gpio.provisionDigitalOutputPin(pin, "LED_"+entry.getKey(), PinState.HIGH);
             gpioPin.setShutdownOptions(true, PinState.LOW);
             pins.put(pin.getAddress(), gpioPin);
             System.out.println("PIN: addres:" + pin.getAddress() + " name:" +pin.getName() + " provider:" + pin.getProvider());
-            PinMode mode = gpioPin.getMode();
-            System.out.println("EXTRAS: direction: " +mode.getDirection() + " name:" + mode.getName()  + " value:" + mode.getValue());
-        }
+        });
         System.out.println("PINS Inicializados");
     }
 
-    private static void turnOn(int pin) {
-        validatePin(pin);
-        GpioPinDigitalOutput gpioPin = pins.get(pin);
+    private static void turnOn(Pin pin) {
+        GpioPinDigitalOutput gpioPin = pins.get(pin.getAddress());
         gpioPin.high();
     }
 
-    private static void turnOff(int pin) {
-        validatePin(pin);
-        GpioPinDigitalOutput gpioPin = pins.get(pin);
+    private static void turnOff(Pin pin) {
+        GpioPinDigitalOutput gpioPin = pins.get(pin.getAddress());
         gpioPin.low();
     }
 
-    private static void operatePin(int pin, int timeOut) {
-        GpioPinDigitalOutput gpioPin = pins.get(pin);
+    private static void operatePin(Pin pin, int timeOut) {
+        GpioPinDigitalOutput gpioPin = pins.get(pin.getAddress());
         try {
-            validatePin(pin);
             System.out.println("TURNING OFF");
             gpioPin.low();
             TimeUnit.SECONDS.sleep(timeOut);
